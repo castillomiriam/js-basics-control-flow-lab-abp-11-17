ccb1cf0817acbcb2762eb96f3c1d73b83544631d
Miriam Castillo <castillo.m.mc@gmail.com>
2017-11-18 05:22:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,9 @@
 function scuberGreetingForFeet(distance){
   if (distance <= 400){
     return "This one is on me!";
+  } else if (distance >2000){
+    return 'I will gladly take yout thirty bucks.'
+  } else if (distance > 2500){
+    return 'No can do.'
   }
 }
