b8605148413abb13e4bdc1af57a534adaf06d28f
Miriam Castillo <castillo.m.mc@gmail.com>
2017-11-18 05:09:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-function scuberGreetingForFeet('gives customers a free sample if the ride is less than or equal to 400 feet')  {
+console.log scuberGreetingForFeet('gives customers a free sample if the ride is less than or equal to 400 feet')  {
   if (scuberGreetingForFeet(199)) {
     return 'This one is on me!'
   }
