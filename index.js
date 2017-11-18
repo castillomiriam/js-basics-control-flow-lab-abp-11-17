ba48475305a6dbfa3b2e2ffcb2b2670f77e12c9f
Miriam Castillo <castillo.m.mc@gmail.com>
2017-11-18 05:32:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function scuberGreetingForFeet(distance){
 function ternaryCheckCity(city){
   if (city === "NYC"){
     return "Ok, sounds good."
-  } else if {
+  } else {
     return "No go."
   }
 }
